ee325b9c6fda304861911dcabe1d01ca162f265c
henryx3 <115536144+henryx3@users.noreply.github.com>
2023-01-25 10:48:29 +0100

    Backend Mod

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
-variable "aws_access_key" {
-  type        = string
-  description = "AWS access key"
-}
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS secret key"
-}
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
-}
+# variable "aws_access_key" {
+#   type        = string
+#   description = "AWS access key"
+# }
+# variable "aws_secret_key" {
+#   type        = string
+#   description = "AWS secret key"
+# }
+# variable "aws_region" {
+#   type        = string
+#   description = "AWS region"
+# }
 variable "vpc_cidr_block" {
   description = "IP addressing for Network"
 }
